a6c7a64d025bd72a2ac2112d9044b8a009a9b15e
Ken Española <revas4192@gmail.com>
2026-04-13 20:54:06 +0800

    learned: story fixing

---
+++ Introduction/part1/story.py
@@ -0,0 +1,14 @@
+#this one is hard 
+# expected output: Please type in a name: Mary
+#Please type in a year: 1572
+
+#Mary is a valiant knight, born in the year 1572. One morning Mary woke up to an awful racket: a dragon was approaching the village. Only Mary could save the village's residents.
+
+# Write your solution here
+# given the problem we will insert the name and year on the sentence presented born in the year + year + and only + name +
+name = input("What is your name? ")
+year = input("What is the year? ")
+# we will use the variable name to the name like mary and 1572 
+print( name +" " +"is a valiant knight, born in the year"+" "+year+"." " One morning"+" "+ name +" "+"woke up to an awful racket: a dragon was approaching the village. Only"+" "+name+" "+"could save the village's residents.")
+
+#Reflection: I am getting better at problem solving and analyzing with pseudocode
