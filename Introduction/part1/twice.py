8f07c3d69cca9503f59469ad9b42c366eee9f24e
Ken Española <revas4192@gmail.com>
2026-04-13 19:48:58 +0800

    learned starting strong

---
+++ Introduction/part1/twice.py
@@ -0,0 +1,15 @@
+#Task: print !Ken!Ken!
+
+#what i did at first
+
+name = input("What is your name? ")
+## print("!"+name+"!"+ name "1")
+##which is my first attempt but it didnt work because i forgot to add the + sign between name and "1"
+
+#so I have debugged it and added the + sign and now it works
+
+#this is my second attempt and it works fine
+name = input("What is your name? ")
+print("!"+name+"!"+name+"!")
+
+#what i have learned from this is i take time to look to my code and check it for any mistakes and try to fix it. I also learned that i can use the + sign to concatenate strings in python.
