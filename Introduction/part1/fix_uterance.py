34a53d0b22cb2bd86390551c82f45f7e2650bfd9
Ken Española <revas4192@gmail.com>
2026-04-13 20:09:43 +0800

    learned: debugging

---
+++ Introduction/part1/fix_uterance.py
@@ -0,0 +1,20 @@
+
+#Task: The code below is supposed to print out the nursery rhyme "Hickory Dickory Dock!" but it has some errors. Fix the code so that it works correctly.
+part = input("The 1st part: ")
+part = input("The 1st part: ")
+part = input("The 1st part: ")
+print(part + part + part)
+
+# Fix the code
+# the problem is i must print out hickory at the 1st part dickory at 2nd part and dock at the 3rd part
+#the problem is the variable is the same part
+# First i apply the hypen(-) first at the print function
+part1 = input("The 1st part: ")
+# We will now fix the variables because its the same with other 
+part2 = input("The 2st part: ")
+# I will now change the input print
+part3 = input("The 3st part: ")
+#lastly will include the exclamation mark at the end of the print function
+print(part1+"-"+ part2+"-"+ part3 + "!")
+
+#this test my debugging skills and give me good practice on how to fix errors in my code. I also learned that i have to pay attention to the details and check my code for any mistakes before running it. Overall, this was a good learning experience for me.
