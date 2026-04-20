9e8472c438ee3ce4c478a2582cfae6097d13ddd7
Ken Española <revas4192@gmail.com>
2026-04-20 16:17:25 +0800

    learned: input validation in while loop

---
+++ Introduction/part2/input_validation.py
@@ -0,0 +1,46 @@
+#Please write a program which asks the user for integer numbers.
+
+#If the number is below zero, the program should print out the message "Invalid number".
+
+#If the number is above zero, the program should print out the square root of the number using the Python sqrt function.
+
+#In either case, the program should then ask for another number.
+
+#If the user inputs the number zero, the program should stop asking for numbers and exit the loop.
+
+#Below you'll find a reminder of how the sqrt function is used. Remember to import it in the beginning of the program.
+
+# sqrt function will not work without this line in the beginning of the program
+
+#from math import sqrt
+
+#print(sqrt(9))
+#Sample output
+#3.0
+
+#An example of expected behaviour of your program:
+
+#Sample output
+#Please type in a number: 16
+#4.0
+#Please type in a number: 4
+#2.0
+#Please type in a number: -3
+#Invalid number
+#Please type in a number: 1
+#1.0
+#Please type in a number: 0
+#Exiting...
+
+from math import sqrt
+# Write your solution here
+while True:
+    number = int(input("Please type in a number:"))
+    if number == 0:
+        break
+    #if the number is less than 0 (e.g. -3) it will print invalid but if not it will square the number
+    if number <= 0:
+        print("Invalid number")
+    else:
+        print(sqrt(number))
+print("Exiting...")
