e51d549f35f79a32958ea3379579550f0be5a66d
Ken Española <revas4192@gmail.com>
2026-04-22 11:00:21 +0800

    learned: conditional while loops

---
+++ Introduction/part3/print_numbers.py
@@ -0,0 +1,17 @@
+#Please write a program which prints out all the even numbers between two and thirty, using a loop. Print each number on a separate line.
+
+#The beginning of your output should look like this:
+
+#Sample output
+
+#2
+#4
+#6
+#8
+#etc...
+
+# Write your solution here
+even = 2
+while even <= 30:
+    print(even)
+    even += 2
