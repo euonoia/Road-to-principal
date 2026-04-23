b6031678ec4dd8a58645197d2f7f8a1855720644
Ken Española <revas4192@gmail.com>
2026-04-23 14:48:23 +0800

    learned: longer string

---
+++ Introduction/part3/longer_string.py
@@ -0,0 +1,31 @@
+#Please write a program which asks the user for two strings and then prints out whichever is the longer of the two - that is, whichever has the more characters. If the strings are of equal length, the program should print out "The strings are equally long".
+
+#Some examples of expected behaviour:
+
+#Sample output
+#Please type in string 1: hey
+#Please type in string 2: hiya
+#hiya is longer
+
+#Sample output
+#Please type in string 1: howdy doody
+#Please type in string 2: hola
+#howdy doody is longer
+
+#Sample output
+#Please type in string 1: hey
+#Please type in string 2: bye
+#The strings are equally long
+
+string1 = input("Please type in string 1: ")
+string2 = input("Please type in string 2: ")
+
+lenght1 = len(string1)
+lenght2 = len(string2)
+
+if lenght1 > lenght2:
+    print(f"{string1} is longer")
+elif lenght2 > lenght1:
+    print(f"{string2} is longer")
+else:
+    print("The strings are equally long")
