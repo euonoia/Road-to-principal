93e4c0388637861e3cac8472d91d1f4c934ba3f5
Ken Española <revas4192@gmail.com>
2026-04-26 12:45:08 +0800

    learned: line of hashes

---
+++ Introduction/part3/A _line_of_hashes.py
@@ -0,0 +1,17 @@
+#Please write a program which prints out a line of hash characters, the width of which is chosen by the user.
+
+#Sample output
+#Width: 3
+
+###
+
+#Sample output
+#Width: 8
+
+########
+
+# Write your solution here
+
+width = int(input("Width:"))
+
+print("#" * width)
