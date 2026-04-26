8bd34af9a082796b8158ba266ea242cb6bddf8c2
Ken Española <revas4192@gmail.com>
2026-04-26 16:41:11 +0800

    learned: right alligned

---
+++ Introduction/part3/Right-aligned.py
@@ -0,0 +1,26 @@
+#Please write a program which asks the user for a string and then prints it out so that exactly 20 characters are displayed. If the input is shorter than 20 characters, the beginning of the line is filled in with * characters.
+
+#You may assume the input string is at most 20 characters long.
+
+#Sample output
+#Please type in a string: python
+
+#**************python
+#Sample output
+#Please type in a string: alongerstring
+
+#*******alongerstring
+#Sample output
+#Please type in a string: averyverylongstring
+
+#*averyverylongstring
+
+# Write your solution here
+
+input_string = input("Please type in a string: ")
+
+lenght = len(input_string)
+
+limit = 20 - lenght
+
+print("*" * limit + input_string)
