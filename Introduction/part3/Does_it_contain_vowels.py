462e7ce71251d96cfc0a1c9d44913c240829ad9b
Ken Española <revas4192@gmail.com>
2026-04-27 11:50:05 +0800

    learned: in substring

---
+++ Introduction/part3/Does_it_contain_vowels.py
@@ -0,0 +1,32 @@
+#Please write a program which asks the user to input a string. The program then prints out different messages if the string contains any of the vowels a, e or o.
+
+#You may assume the input will be in lowercase entirely. Have a look at the examples below.
+
+#Sample output
+#Please type in a string: hello there
+#a not found
+#e found
+#o found
+
+#Sample output
+#Please type in a string: hiya
+#a found
+#e not found#
+# o not found
+
+# Write your solution here
+
+input_string = input("Please type in a string:")
+
+if "a" in input_string:
+    print("a found")
+else:
+    print("a not found")
+if "e" in input_string:
+    print("e found")
+else:
+    print("e not found")
+if "o" in input_string:
+    print("o found")
+else:
+    print("o not found")
