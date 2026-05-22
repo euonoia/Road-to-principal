5c4877ca2fdaeea6a21bcd6b0edec869c81e3166
Ken Española <revas4192@gmail.com>
2026-05-22 11:52:53 +0800

    learned: age

--- javascript/age.js
+++ javascript/age.js
@@ -1,9 +1,14 @@
 const name = "Alice";
-let age = 15;
+let age = 5;
 
-if (age >= 18){
-    console.log(name + " is an Adult.");
-}
-else if (age <= 17){
-    console.log(name + " Is a minor.");
+if (age >= 0 && age <= 120){
+    if (age >= 18){
+        console.log(name + " is an Adult.");
+    }
+    else if (age <= 17){
+        console.log(name + " Is a minor.");
+    }
+} 
+else {
+    console.log("Invalid age.");
 }
