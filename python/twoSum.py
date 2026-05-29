e6f6abc33d45ddeecdeae1ea2ec8dc17f11c95cb
Ken Española <revas4192@gmail.com>
2026-05-29 14:17:51 +0800

    Exercise:TwoSum

--- python/twoSum.py
+++ python/twoSum.py
@@ -3,7 +3,5 @@ def twoSum(arr, target):
         for j in range(len(arr)):
             if i != j:
                 if arr[i] + arr[j] == target:
-                    print(f"{arr[i]} + {arr[j]} = {target}")
-                    return True
-    return False
+                    return print([i,j])
 twoSum([3,2,4],6)
